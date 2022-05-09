d61bced908bf55e7f803146e6e014b034014c581
Davidpurtill1101011 <davidpurtill91@outlook.com>
2022-05-09 20:52:11 +0100

    Added code so the leap can be tracked on the screen

--- LeapMotionMouse/src/ie/atu/sw/LeapMouse.java
+++ LeapMotionMouse/src/ie/atu/sw/LeapMouse.java
@@ -1,8 +1,24 @@
 package ie.atu.sw;
 
+import java.io.IOException;
+
+import com.leapmotion.leap.Controller;
+
 public class LeapMouse {
 	public static void main(String[] args) {
-		System.out.println("Test");
+		Listeners l = new Listeners();
+		Controller c = new Controller();
+		c.addListener(l);
+		
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		c.removeListener(l);
+		
 	}
 
 }
--- LeapMotionMouse/src/ie/atu/sw/Listeners.java
+++ LeapMotionMouse/src/ie/atu/sw/Listeners.java
@@ -1,5 +1,41 @@
 package ie.atu.sw;
 
-public class Listeners {
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Finger;
+import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.Gesture;
+import com.leapmotion.leap.InteractionBox;
+import com.leapmotion.leap.Listener;
+import com.leapmotion.leap.Vector;
+import java.awt.*;
 
+public class Listeners extends Listener {
+	
+	public Robot r;
+	public void onConnect(Controller c) {
+		c.enableGesture(Gesture.Type.TYPE_CIRCLE);
+		c.enableGesture(Gesture.Type.TYPE_SWIPE);
+		c.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+		
+	}
+	
+	public void onFrame(Controller c) {
+		
+		try {
+			r = new Robot();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Frame frame = c.frame();
+		InteractionBox ib = frame.interactionBox();
+		for(Finger f : frame.fingers()) {
+			if(f.type() == Finger.Type.TYPE_INDEX) {
+				Vector fingerPos = f.tipPosition();
+				Vector boxFingerPos = ib.normalizePoint(fingerPos);
+				Dimension screen = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+				r.mouseMove((int)(screen.width * boxFingerPos.getX()), (int)(screen.height - boxFingerPos.getY() * screen.height));
+			}
+		}
+	}
 }
